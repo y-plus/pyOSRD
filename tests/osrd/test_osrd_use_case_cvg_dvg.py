144c455279f2e138a390f5e08c5c30fd0eb69028
Renan HILBERT <renan.hilbert@eurodecision.com>
2023-06-05 12:05:15 +0200

    Use OSRD.use_case in tests

--- tests/osrd/test_osrd_use_case_cvg_dvg.py
+++ tests/osrd/test_osrd_use_case_cvg_dvg.py
@@ -5,11 +5,11 @@ osrd_cvg_dvg
 
 station0 (2 tracks)                        station1 (2 tracks)
 
-(T0)--S0-D0-                                -S4-D4-(T4)-->
-            \                              /
-        (CVG)>-(T2)---------o--S3-D3-(T3)-<(DVG)
-            /                              \
-(T1)--S1-D1-                                -S5-D5-(T5)-->
+(T0)--S0-D0-                                  -S4-D4-(T4)-->
+                \                              /
+            (CVG)>-(T2)---------o--S3-D3-(T3)-<(DVG)
+                /                              \
+(T1)--S1-D1-                                  -S5-D5-(T5)-->
 
 All tracks are 500m long
 Train 0 starts from T0 at t=0 and arrives at T4
@@ -20,12 +20,12 @@ import pytest
 import matplotlib.pyplot as plt
 
 
-def test_osrd_infra(osrd_cvg_dvg):
-    assert isinstance(osrd_cvg_dvg.infra, dict)
+def testuse_case_cvg_dvg_infra(use_case_cvg_dvg):
+    assert isinstance(use_case_cvg_dvg.infra, dict)
 
 
-def test_osrd_infra_routes(osrd_cvg_dvg):
-    assert set(osrd_cvg_dvg.routes) == \
+def testuse_case_cvg_dvg_infra_routes(use_case_cvg_dvg):
+    assert set(use_case_cvg_dvg.routes) == \
         set([
             'rt.D0->D3',
             'rt.D1->D3',
@@ -38,8 +38,8 @@ def test_osrd_infra_routes(osrd_cvg_dvg):
         ])
 
 
-def test_osrd_infra_route_switches(osrd_cvg_dvg):
-    assert osrd_cvg_dvg.route_switches == \
+def testuse_case_cvg_dvg_infra_route_switches(use_case_cvg_dvg):
+    assert use_case_cvg_dvg.route_switches == \
         {
             'rt.D0->D3': 'CVG',
             'rt.D1->D3': 'CVG',
@@ -48,8 +48,8 @@ def test_osrd_infra_route_switches(osrd_cvg_dvg):
         }
 
 
-def test_osrd_infra_route_limits(osrd_cvg_dvg):
-    assert osrd_cvg_dvg.route_limits == \
+def testuse_case_cvg_dvg_infra_route_limits(use_case_cvg_dvg):
+    assert use_case_cvg_dvg.route_limits == \
         {
             'D0': ('T0', 450.0),
             'D1': ('T1', 450.0),
@@ -63,8 +63,8 @@ def test_osrd_infra_route_limits(osrd_cvg_dvg):
         }
 
 
-def test_osrd_infra_block_lengths(osrd_cvg_dvg):
-    assert osrd_cvg_dvg.track_section_lengths == \
+def testuse_case_cvg_dvg_infra_block_lengths(use_case_cvg_dvg):
+    assert use_case_cvg_dvg.track_section_lengths == \
         {
             'T0': 500.0,
             'T1': 500.0,
@@ -75,8 +75,8 @@ def test_osrd_infra_block_lengths(osrd_cvg_dvg):
         }
 
 
-def test_osrd_infra_route_lengths(osrd_cvg_dvg):
-    assert osrd_cvg_dvg.route_lengths == \
+def testuse_case_cvg_dvg_infra_route_lengths(use_case_cvg_dvg):
+    assert use_case_cvg_dvg.route_lengths == \
         {
             'rt.D0->D3': 1000.0,
             'rt.buffer_stop.0->D0': 450.0,
@@ -89,38 +89,38 @@ def test_osrd_infra_route_lengths(osrd_cvg_dvg):
         }
 
 
-def test_osrd_infra_num_switches(osrd_cvg_dvg):
-    assert osrd_cvg_dvg.num_switches == 2
+def testuse_case_cvg_dvg_infra_num_switches(use_case_cvg_dvg):
+    assert use_case_cvg_dvg.num_switches == 2
 
 
-def test_osrd_infra_draw_infra_not_fail(osrd_cvg_dvg):
+def testuse_case_cvg_dvg_infra_draw_infra_not_fail(use_case_cvg_dvg):
     """Test if it does not raise an exception"""
     try:
-        osrd_cvg_dvg.draw_infra()
+        use_case_cvg_dvg.draw_infra()
     except:  # noqa
         assert False
 
 
-def test_osrd_infra_points_of_interest(osrd_cvg_dvg):
-    poi = osrd_cvg_dvg.points_of_interest
+def testuse_case_cvg_dvg_infra_points_of_interest(use_case_cvg_dvg):
+    poi = use_case_cvg_dvg.points_of_interest
     assert set(poi.keys()) == {'CVG', 'DVG', 'station0', 'station1'}
 
 
-def test_osrd_infra_station_capacities(osrd_cvg_dvg):
+def testuse_case_cvg_dvg_infra_station_capacities(use_case_cvg_dvg):
     assert (
-        osrd_cvg_dvg.station_capacities == {'station0': 2, 'station1': 2}
+        use_case_cvg_dvg.station_capacities == {'station0': 2, 'station1': 2}
     )
 
 
-def test_osrd_infra_num_stations(osrd_cvg_dvg):
-    assert osrd_cvg_dvg.num_stations == 2
+def testuse_case_cvg_dvg_infra_num_stations(use_case_cvg_dvg):
+    assert use_case_cvg_dvg.num_stations == 2
 
 
-def test_osrd_convergence_entry_signals(osrd_cvg_dvg):
-    assert osrd_cvg_dvg.convergence_entry_signals == ['S0', 'S1']
+def testuse_case_cvg_dvg_convergence_entry_signals(use_case_cvg_dvg):
+    assert use_case_cvg_dvg.convergence_entry_signals == ['S0', 'S1']
 
 
-def test_osrd_points_on_tracks(osrd_cvg_dvg):
+def testuse_case_cvg_dvg_points_on_tracks(use_case_cvg_dvg):
     expected = {
         "T0": {
             "station0": (300, 'station'),
@@ -149,55 +149,55 @@ def test_osrd_points_on_tracks(osrd_cvg_dvg):
         },
     }
 
-    assert osrd_cvg_dvg.points_on_track_sections == expected
+    assert use_case_cvg_dvg.points_on_track_sections == expected
 
 
-def test_osrd_simulation_type(osrd_cvg_dvg):
-    assert isinstance(osrd_cvg_dvg.simulation, dict)
+def testuse_case_cvg_dvg_simulation_type(use_case_cvg_dvg):
+    assert isinstance(use_case_cvg_dvg.simulation, dict)
 
 
-def test_osrd_simulation_num_trains(osrd_cvg_dvg):
-    assert osrd_cvg_dvg.num_trains == 2
+def testuse_case_cvg_dvg_simulation_num_trains(use_case_cvg_dvg):
+    assert use_case_cvg_dvg.num_trains == 2
 
 
-def test_osrd_simulation_trains(osrd_cvg_dvg):
-    assert osrd_cvg_dvg.trains == ['train0', 'train1']
+def testuse_case_cvg_dvg_simulation_trains(use_case_cvg_dvg):
+    assert use_case_cvg_dvg.trains == ['train0', 'train1']
 
 
-def test_osrd_simulation_departure_times(osrd_cvg_dvg):
-    assert osrd_cvg_dvg.departure_times == [0, 100]
+def testuse_case_cvg_dvg_simulation_departure_times(use_case_cvg_dvg):
+    assert use_case_cvg_dvg.departure_times == [0, 100]
 
 
-def test_osrd_run_arror(osrd_cvg_dvg_missing_sim):
+def testuse_case_cvg_dvg_run_arror(osrd_cvg_dvg_missing_sim):
     match = "Missing json file to run OSRD"
     with pytest.raises(ValueError, match=match):
         osrd_cvg_dvg_missing_sim.run()
 
 
-def test_osrd_has_results(osrd_cvg_dvg):
-    assert osrd_cvg_dvg.has_results
+def testuse_case_cvg_dvg_has_results(use_case_cvg_dvg):
+    assert use_case_cvg_dvg.has_results
 
 
-def test_osrd_has_no_results(osrd_cvg_dvg_before_run):
+def testuse_case_cvg_dvg_has_no_results(osrd_cvg_dvg_before_run):
     assert not osrd_cvg_dvg_before_run.has_results
 
 
-def test_osrd_results_length(osrd_cvg_dvg):
-    assert len(osrd_cvg_dvg.results) == osrd_cvg_dvg.num_trains
+def testuse_case_cvg_dvg_results_length(use_case_cvg_dvg):
+    assert len(use_case_cvg_dvg.results) == use_case_cvg_dvg.num_trains
 
 
-def test_osrd_results_train_track_sections(osrd_cvg_dvg):
-    assert osrd_cvg_dvg.train_track_sections(0) == ['T0', 'T2', 'T3', 'T4']
-    assert osrd_cvg_dvg.train_track_sections(1) == ['T1', 'T2', 'T3', 'T5']
+def testuse_case_cvg_dvg_results_train_track_sections(use_case_cvg_dvg):
+    assert use_case_cvg_dvg.train_track_sections(0) == ['T0', 'T2', 'T3', 'T4']
+    assert use_case_cvg_dvg.train_track_sections(1) == ['T1', 'T2', 'T3', 'T5']
 
 
-def test_osrd_results_points_encountered_by_train(osrd_cvg_dvg):
+def testuse_case_cvg_dvg_results_points_encountered_by_train(use_case_cvg_dvg):
     points = [
         {
             k: v for k, v in d.items()
             if k not in ['t', 't_min']
         }
-        for d in osrd_cvg_dvg.points_encountered_by_train(0)
+        for d in use_case_cvg_dvg.points_encountered_by_train(0)
     ]
     expected = [
         {'id': 'station0', 'type': 'station', 'offset': 300.0},
@@ -212,9 +212,9 @@ def test_osrd_results_points_encountered_by_train(osrd_cvg_dvg):
     assert points == expected
 
 
-def test_osrd_space_time_graph(osrd_cvg_dvg):
+def testuse_case_cvg_dvg_space_time_graph(use_case_cvg_dvg):
 
-    ax = osrd_cvg_dvg.space_time_graph(0, types_to_show=['station'])
+    ax = use_case_cvg_dvg.space_time_graph(0, types_to_show=['station'])
 
     assert ax.dataLim.xmin == 0.
     assert round(ax.dataLim.ymin) == 300.
