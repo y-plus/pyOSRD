b95bf47dda73158d7c11c32b1db333b916c010e2
Renan HILBERT <renan.hilbert@eurodecision.com>
2023-06-18 11:40:34 +0200

    Add interval intersections helper function

---
+++ tests/maths/test_intervals.py
@@ -0,0 +1,25 @@
+from rlway.maths.intervals import intersections
+
+
+def test_interval_intersections():
+    """cf test_interval_intersections.png"""
+
+    fixed_intervals = [
+        (1, 2),
+        (3, 4),
+        ]
+    test_intervals = {
+        (0, .5): [],
+        (0, 1.5): [(1, 1.5)],
+        (1.2, 1.8): [(1.2, 1.8)],
+        (1.5, 2.5): [(1.5, 2)],
+        (2.2, 2.8): [],
+        (1.5, 3.5): [(1.5, 2), (3, 3.5)],
+        (2.5, 3.5): [(3., 3.5)],
+        (3.5, 3.8): [(3.5, 3.8)],
+        (3.5, 4.5): [(3.5, 4)],
+        (4.5, 5): [],
+    }
+
+    for interval, expected in test_intervals.items():
+        assert intersections(fixed_intervals+[interval]) == expected
