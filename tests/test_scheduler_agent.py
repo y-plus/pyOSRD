901b1031e4aff95998bf82944251e0825bed98e2
Renan HILBERT <renan.hilbert.ext@eurodecision.com>
2024-02-11 13:42:49 +0100

    [scheduler_agent] moved to agents/

--- tests/test_scheduler_agent.py
+++ tests/test_scheduler_agent.py
@@ -4,7 +4,7 @@ import pandas as pd
 from pandas.testing import assert_frame_equal
 
 from pyosrd import OSRD
-from pyosrd.scheduler_agent import SchedulerAgent
+from pyosrd.agents.scheduler_agent import SchedulerAgent
 
 
 def test_scheduler_agent_autonomous(two_trains):
