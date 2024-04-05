3289c2426351395dd4bb1bbf4d5ceaa418457ce3
Renan HILBERT <renan.hilbert.ext@eurodecision.com>
2024-04-05 18:36:52 +0200

    [OSRD] Upgrade methods to v0.2.9

--- src/pyosrd/osrd.py
+++ src/pyosrd/osrd.py
@@ -225,20 +225,9 @@ class OSRD():
         ]
 
     @property
-    def switches(self) -> list[dict[str, Any]]:
-        """ List of switches (track section links excluded)"""
-        return [
-            switch for switch in self.infra['switches']
-            if switch['switch_type'] != 'link'
-        ]
-
-    @property
     def num_switches(self) -> int:
         """Number of switches"""
-        return len([
-            switch for switch in self.infra['switches']
-            if switch['switch_type'] != 'link'
-        ])
+        return len(self.switches)
 
     @property
     def station_capacities(self) -> dict[str, int]:
