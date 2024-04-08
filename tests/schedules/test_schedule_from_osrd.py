526b8b58b61ce732099507f7b65e16d579835b78
Renan HILBERT <renan.hilbert.ext@eurodecision.com>
2024-04-08 17:22:03 +0200

    [schedules] test from_osrd step_type

--- tests/schedules/test_schedule_from_osrd.py
+++ tests/schedules/test_schedule_from_osrd.py
@@ -1,3 +1,8 @@
+import numpy as np
+import pandas as pd
+
+from pandas.testing import assert_frame_equal
+
 from pyosrd.schedules import schedule_from_osrd
 
 
@@ -13,7 +18,6 @@ def test_schedule_from_osrd(use_case_cvg_dvg):
         'D5<->buffer_stop.3',
     ])
     assert list(s._df.columns.levels[0]) == ['train0', 'train1']
-    assert s.trains == use_case_cvg_dvg.trains
 
 
 def test_schedule_from_osrd_merge_switch_zones(use_case_double_switch):
@@ -21,3 +25,29 @@ def test_schedule_from_osrd_merge_switch_zones(use_case_double_switch):
 
     assert len(s.zones) == 5
     assert 'SW0+SW1' in s.zones
+
+
+def test_schedule_from_osrd_trains(use_case_cvg_dvg):
+    s = schedule_from_osrd(use_case_cvg_dvg)
+    assert s._trains == use_case_cvg_dvg.trains
+    assert s.trains == use_case_cvg_dvg.trains
+
+
+def test_schedule_from_osrd_step_type(use_case_station_capacity2):
+    s = schedule_from_osrd(use_case_station_capacity2)
+    expected = pd.DataFrame(
+        {
+            0: ['signal', 'switch', 'station', np.nan, 'switch', 'last_zone'],
+            1: ['signal', 'switch', np.nan, 'station', 'switch', 'last_zone'],
+        },
+        index=[
+            'D0<->buffer_stop.0',
+            'DVG',
+            'D1<->D3',
+            'D2<->D4',
+            'CVG',
+            'D5<->buffer_stop.5',
+        ]
+    )
+    assert_frame_equal(s._step_type, expected)
+    assert_frame_equal(s.step_type, expected)
