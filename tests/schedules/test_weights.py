b4cb15863ad8b49c591bd9eb044ce2c011542100
Renan HILBERT <renan.hilbert.ext@eurodecision.com>
2024-04-11 14:32:30 +0200

    [weights+from_osrd] Fix problemes calling trains by id

--- tests/schedules/test_weights.py
+++ tests/schedules/test_weights.py
@@ -10,8 +10,8 @@ def test_weights_all_steps(use_case_station_capacity2):
 
     expected = pd.DataFrame(
         {
-            0: [1, 1, 1, 0, 1, 1],
-            1: [1, 1, 0, 1, 1, 1],
+            'train0': [1, 1, 1, 0, 1, 1],
+            'train1': [1, 1, 0, 1, 1, 1],
         },
         index=schedule_from_osrd(use_case_station_capacity2).df.index
     )
@@ -24,8 +24,8 @@ def test_weights_stations_only(use_case_station_capacity2):
 
     expected = pd.DataFrame(
         {
-            0: [0, 0, 1, 0, 0, 0],
-            1: [0, 0, 0, 1, 0, 0],
+            'train0': [0, 0, 1, 0, 0, 0],
+            'train1': [0, 0, 0, 1, 0, 0],
         },
         index=schedule_from_osrd(use_case_station_capacity2).df.index
     )
@@ -38,8 +38,8 @@ def test_weights_set_for_one_train(use_case_station_capacity2):
     w.weights.train(0, 2)
     expected = pd.DataFrame(
         {
-            0: [2, 2, 2, 0, 2, 2],
-            1: [1, 1, 0, 1, 1, 1],
+            'train0': [2, 2, 2, 0, 2, 2],
+            'train1': [1, 1, 0, 1, 1, 1],
         },
         index=schedule_from_osrd(use_case_station_capacity2).df.index
     )
@@ -52,8 +52,8 @@ def test_weights_set_for_one_step(use_case_station_capacity2):
     w.weights.train_zone(0, 'DVG', 2)
     expected = pd.DataFrame(
         {
-            0: [1, 2, 1, 0, 1, 1],
-            1: [1, 1, 0, 1, 1, 1],
+            'train0': [1, 2, 1, 0, 1, 1],
+            'train1': [1, 1, 0, 1, 1, 1],
         },
         index=schedule_from_osrd(use_case_station_capacity2).df.index
     )
@@ -67,8 +67,8 @@ def test_weights_set_for_one_train_at_a_station(use_case_station_capacity2):
     w.weights.train_station_sim(1, 'station', 2, use_case_station_capacity2)
     expected = pd.DataFrame(
         {
-            0: [1, 1, 2, 0, 1, 1],
-            1: [1, 1, 0, 2, 1, 1],
+            'train0': [1, 1, 2, 0, 1, 1],
+            'train1': [1, 1, 0, 2, 1, 1],
         },
         index=schedule_from_osrd(use_case_station_capacity2).df.index
     )
