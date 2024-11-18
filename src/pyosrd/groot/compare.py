b3aa5db9499bc243211c38dc08856485d1b7d838
Renan HILBERT <renan.hilbert.ext@eurodecision.com>
2024-11-18 11:53:08 +0100

    [Groot] Rerouted paths btw 2 schedules

--- src/pyosrd/groot/compare.py
+++ src/pyosrd/groot/compare.py
@@ -1,3 +1,4 @@
+import itertools
 import math
 from pyosrd.groot import Groot
 
@@ -14,7 +15,7 @@ def difference_durations(g1: Groot, g2: Groot) -> dict[str, dict[str, float]]:
                 if not math.isclose(d1, d2):
                     if train not in diff:
                         diff[train] = dict()
-                    diff[train][tvd] = (round(d2 - d1, 0))
+                    diff[train][tvd] = round(d2 - d1, 0)
 
     return diff
 
@@ -26,12 +27,27 @@ def difference_departures(g1: Groot, g2: Groot) -> dict[str, float]:
     for train in g1.times:
         tvd = g1.path(train)[0]
         if (
-            tvd == g2.path(train)[0] 
+            tvd == g2.path(train)[0]
             and not math.isclose(g1.times[train][tvd][0], g2.times[train][tvd][0])
         ):
             diff[train] = g2.times[train][tvd][0] - g1.times[train][tvd][0]
     return diff
 
 
-def difference_path(g1: Groot, g2: Groot) -> dict[str, dict[str, float]]:
-    ...
+def rerouted_paths(g: Groot, ref: Groot) -> dict[str, list[list[str]]]:
+
+    rerouted = dict()
+
+    for train in g.trains:
+        new_tvds = [tvd if tvd not in ref.path(train) else None for tvd in g.path(train) ]
+
+        new_paths = [
+            list(v)
+            for k, v in itertools.groupby(new_tvds, key=lambda x: x is None)
+            if not k
+        ]
+
+        if new_paths:
+            rerouted[train] = new_paths
+
+    return rerouted
