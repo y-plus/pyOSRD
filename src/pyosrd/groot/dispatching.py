7f3578fb1a7a12501b25f92724dc967c0ca45697
Renan HILBERT <renan.hilbert.ext@eurodecision.com>
2024-11-20 07:55:53 +0100

    [groot/dispatching] update actions

--- src/pyosrd/groot/dispatching.py
+++ src/pyosrd/groot/dispatching.py
@@ -1,11 +1,11 @@
 from typing import Callable
 from pyosrd.groot import Groot
-
+from pyosrd.groot.objectives import sum_delays_at_end
 def evaluate_action(
     self: Groot,
     a: int,
     ref: Groot,
-    scorer: Callable[[Groot, Groot], float],
+    scorer: Callable[[Groot, Groot], float] = sum_delays_at_end,
 ) -> tuple[Groot, bool, bool, float]:
     
     train1, train2, zone, time = self.earliest_conflict()
@@ -20,6 +20,7 @@ def evaluate_action(
             r = self.make_train_wait(waiting_train, priority_train, wait_at, zone)
             valid = True
             done = not r.has_conflicts()
+            
         case 1:  # keep order, wait at previous station
             priority_train = train1
             waiting_train = train2
@@ -63,19 +64,6 @@ def evaluate_action(
             r = self.make_train_wait(waiting_train, priority_train, wait_at, zone)
             valid = True
             done = not r.has_conflicts()    
-        case 5:  # reroute first train
-            if not (alt_zones := self.alternative_zones(train1, zone)):
-                return self, True, False, scorer(self, ref)
-            for alt_zones in alt_zones:
-                if self.zones_are_free(
-                    alt_zones[1:-1],
-                    self.times_zones[train1][alt_zones[0]][0],
-                    self.times_zones[train1][alt_zones[-1]][-1]
-                ):
-                    r = self.reroute(train1, alt_zones)
-                    valid = True
-                    done = not r.has_conflicts()
-                    break
         case _:
             return None, None, None, None
     return r, done, valid, scorer(r, ref)
