1274a826e66786cf402747536987b2c602d7203a
Renan HILBERT <renan.hilbert.ext@eurodecision.com>
2025-01-30 16:38:35 +0100

    [agents] Remove lazy agent and reincorporate it in GrootAgent

--- src/pyosrd/agents/groot_agent.py
+++ src/pyosrd/agents/groot_agent.py
@@ -3,17 +3,18 @@ import copy
 from abc import ABC, abstractmethod
 from typing_extensions import Self
 
-from pyosrd.groot import Groot, from_sim
-from pyosrd.agents.update import sim_with_updated_results
-from pyosrd.groot.objectives import sum_delays_at_end
 from pyosrd.utils import seconds_to_hour
+from pyosrd.groot import Groot, from_sim
+# from pyosrd.agents.update import sim_with_updated_results
+from pyosrd.groot.objectives import sum_delays_at_end
+from pyosrd.groot.dispatching import evaluate_action
 
 
 class GrootAgent(ABC):
 
     name: str
-    ref_groot: Groot | None = None
-    _dispatched_groot: None
+    _dispatched_groot = None
+    _interlocking_groot = None
 
     def __init__(self, name: str, sim, debug: bool = False) -> None:
         self._name = name
@@ -55,21 +56,65 @@ class GrootAgent(ABC):
         if not self._dispatched_groot:
             self._dispatched_groot = self.calculate_dispatch(self._debug)
         return self._dispatched_groot
-
-    def regulated(
-        self: Self,
-    ):
-        return sim_with_updated_results(self.sim, self.dispatched_groot.times, self.name)
     
     @abstractmethod
     def calculate_dispatch(self: Self, debug: bool) -> Groot:
         pass
+    
+    @property
+    def interlocking_groot(self) -> Groot:
+        if self._debug and self._interlocking_groot:
+            print('Interlocking Groot is read in cache.')
+        if not self._interlocking_groot:
+            self._interlocking_groot = self.calculate_interlocking(self._debug)
+        return self._interlocking_groot
+
+    def calculate_interlocking(self: Self, debug: bool) -> Groot:
+        interlocking_groot =  copy.deepcopy(self.disrupted_groot)
+        done = False
+        self.interlocking_actions = []
+        while not done:
+            interlocking_groot, info = evaluate_action(
+                interlocking_groot,
+                a=0,
+                ref=self.ref_groot,
+                scorer=self._scorer
+            )
+            done = info['done']
+            self.interlocking_actions.append(info)
+        for i, action in enumerate(self.interlocking_actions):
+                if i > 0:
+                    added_score = action['score'] - self.interlocking_actions[i-1]['score']
+                else:
+                    added_score = action['score'] -self._scorer(self.disrupted_groot, self.ref_groot)    
+                added_delay = seconds_to_hour(added_score)
+                self.interlocking_actions[i] = {
+                    **action,
+                    'added_score': added_score,
+                    'delay': seconds_to_hour(action['score']),
+                    'added_delay': added_delay
+                }
+        return interlocking_groot
+
+    # def regulated(
+    #     self: Self,
+    # ):
+    #     return sim_with_updated_results(self.sim, self.dispatched_groot.times, self.name)
 
     def clear_cache(self: Self) -> None:
+        self._interlocking_groot = None
+        self.interlocking_actions = None
         self._dispatched_groot = None
+        self.actions = None
 
     def score(self: Self, formatted: bool = False) -> float:
         s = self._scorer(self.dispatched_groot, self._ref_groot)
+        if formatted:
+            return(seconds_to_hour(s).split('.')[0])
+        return s
+
+    def interlocking_score(self: Self, formatted: bool = False) -> float:
+        s = self._scorer(self.interlocking_groot, self._ref_groot)
         if formatted:
             return(seconds_to_hour(s).split('.')[0])
         return s
