d3ae5a8f36f0ea8fec693093ff54cb9151dfa32a
Renan HILBERT <renan.hilbert.ext@eurodecision.com>
2025-03-12 08:45:00 +0100

    [Groot] re-integrate speedup using ortools

--- src/pyosrd/groot/groot.py
+++ src/pyosrd/groot/groot.py
@@ -9,7 +9,7 @@ import networkx as nx
 import pandas as pd
 
 from matplotlib.axes._axes import Axes
-# from ortools.linear_solver import pywraplp
+from ortools.linear_solver import pywraplp
 
 from pyosrd.utils import seconds_to_hour
 
@@ -481,51 +481,51 @@ class Groot(object):
             return
         return trains[idx+1]
 
-    # def speedup(self: Self, ref: Self, train: str, zone: str) -> Self:
+    def speedup(self: Self, ref: Self, train: str, zone: str) -> Self:
 
-    #     reaccelerated = copy.deepcopy(self)
-    #     reaccelerated._times_zones = None
+        reaccelerated = copy.deepcopy(self)
+        reaccelerated._times_zones = None
 
-    #     solver = pywraplp.Solver.CreateSolver("GLOP")
-    #     if not solver:
-    #         return
+        solver = pywraplp.Solver.CreateSolver("GLOP")
+        if not solver:
+            return
 
-    #     t_in, t_out = dict(), dict()
-    #     start_tvd = self.get_tvd(train, zone)
+        t_in, t_out = dict(), dict()
+        start_tvd = self.get_tvd(train, zone)
 
-    #     path = self.path(train)
-    #     steps = path[path.index(start_tvd):]
+        path = self.path(train)
+        steps = path[path.index(start_tvd):]
 
-    #     for i, tvd in enumerate(steps):
-    #         t_in_ref, t_out_ref = ref.times[train][tvd]
-    #         t_in_d, _ = self.times[train][tvd]
+        for i, tvd in enumerate(steps):
+            t_in_ref, t_out_ref = ref.times[train][tvd]
+            t_in_d, _ = self.times[train][tvd]
 
-    #         t_in_min = t_in_ref
-    #         if prev_train:= self.previous_train(train, self.zones[tvd]):
-    #             t_in_min = max(
-    #                 t_in_min,
-    #                 self.times_zones[prev_train][self.zones[tvd]][1]
-    #             )
-    #         t_in[tvd] = solver.NumVar(t_in_min, solver.infinity(), f"t_in_{tvd}")
-    #         t_out[tvd] = solver.NumVar(t_out_ref, solver.infinity(), f"t_out_{tvd}")
-    #         solver.Add(t_out[tvd] - t_in[tvd] >= self.min_durations[train][tvd])
-    #         if i==0:
-    #             solver.Add (t_in[tvd] == self.times[train][tvd][0])
-    #         else:
-    #             previous_tvd = self.path(train)[self.path(train).index(tvd)-1]
-    #             solver.Add(
-    #                 t_in[tvd] ==
-    #                 t_out[previous_tvd]
-    #                 - (self.times[train][previous_tvd][1] - t_in_d)
-    #             )
+            t_in_min = t_in_ref
+            if prev_train:= self.previous_train(train, self.zones[tvd]):
+                t_in_min = max(
+                    t_in_min,
+                    self.times_zones[prev_train][self.zones[tvd]][1]
+                )
+            t_in[tvd] = solver.NumVar(t_in_min, solver.infinity(), f"t_in_{tvd}")
+            t_out[tvd] = solver.NumVar(t_out_ref, solver.infinity(), f"t_out_{tvd}")
+            solver.Add(t_out[tvd] - t_in[tvd] >= self.min_durations[train][tvd])
+            if i==0:
+                solver.Add (t_in[tvd] == self.times[train][tvd][0])
+            else:
+                previous_tvd = self.path(train)[self.path(train).index(tvd)-1]
+                solver.Add(
+                    t_in[tvd] ==
+                    t_out[previous_tvd]
+                    - (self.times[train][previous_tvd][1] - t_in_d)
+                )
 
-    #     solver.Minimize(t_in[steps[-1]])
-    #     status = solver.Solve()
-    #     if status == pywraplp.Solver.OPTIMAL:
-    #         for tvd in steps:
-    #             reaccelerated.times[train][tvd] = (
-    #                 t_in[tvd].solution_value(),
-    #                 t_out[tvd].solution_value()
-    #             )
+        solver.Minimize(t_in[steps[-1]])
+        status = solver.Solve()
+        if status == pywraplp.Solver.OPTIMAL:
+            for tvd in steps:
+                reaccelerated.times[train][tvd] = (
+                    t_in[tvd].solution_value(),
+                    t_out[tvd].solution_value()
+                )
 
-    #     return reaccelerated
+        return reaccelerated
