755d2e9748f463f06782f15e66ef28b3a0db57d9
Renan HILBERT <renan.hilbert.ext@eurodecision.com>
2025-03-25 16:14:08 +0100

    [groot2] make types availble at module level

--- src/pyosrd/groot2/__init__.py
+++ src/pyosrd/groot2/__init__.py
@@ -1,3 +1,5 @@
 from .groot import Groot as Groot
+from .groot import GrootTimes as GrootTimes
+from .groot import GrootDurations as GrootDurations
 from .constructor.constructor import from_sim as from_sim
 from .delays_chart import plot_groot_delays as plot_groot_delays
